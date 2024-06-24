8588c9f7a4ccd99fa35dec78822c829c2336b99c
Miguel <m.mirim1357@gmail.com>
2024-06-24 13:53:58 +0900

    setup_and_run.bat

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -1,19 +1,31 @@
 @echo off
-echo Installing dependencies for the Document Summarization App...
+echo Setting up the Document Summarization App...
+
+REM Create and activate virtual environment
+python -m venv venv
+call venv\Scripts\activate
 
 REM Upgrade pip
-python -m ensurepip --upgrade
 python -m pip install --upgrade pip
 
 REM Install required libraries
-python -m pip install spacy networkx scikit-learn docx2txt PyPDF2 pyinstaller
+pip install spacy networkx scikit-learn docx2txt PyPDF2 pyinstaller
 
 REM Download spaCy language models
 python -m spacy download ja_core_news_sm
 python -m spacy download en_core_web_sm
 
+REM Get spaCy installation path
+for /f "tokens=*" %%i in ('python -c "import spacy; print(spacy.__path__[0])"') do set SPACY_PATH=%%i
+
+REM Create executable
+pyinstaller --onefile --windowed --add-data "%SPACY_PATH%\lang\ja;spacy\lang\ja" --add-data "%SPACY_PATH%\lang\en;spacy\lang\en" --hidden-import="spacy" --hidden-import="spacy.lang.ja" --hidden-import="spacy.lang.en" SummaryApp.py
+
 echo.
-echo Installation complete!
-echo If you encountered any errors, please check the error messages and try to resolve them manually.
+echo Setup complete! The executable has been created in the 'dist' folder.
 echo.
+
+REM Run the executable
+start "" "dist\SummaryApp.exe"
+
 pause
