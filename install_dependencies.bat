42984c1215f5171b03f839803c1355f8b0e0c089
Miguel <m.mirim1357@gmail.com>
2024-06-24 13:24:11 +0900

    install_dependencies.bat

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -2,10 +2,11 @@
 echo Installing dependencies for the Document Summarization App...
 
 REM Upgrade pip
+python -m ensurepip --upgrade
 python -m pip install --upgrade pip
 
 REM Install required libraries
-pip install spacy networkx scikit-learn docx2txt PyPDF2 pyinstaller
+python -m pip install spacy networkx scikit-learn docx2txt PyPDF2 pyinstaller
 
 REM Download spaCy language models
 python -m spacy download ja_core_news_sm
